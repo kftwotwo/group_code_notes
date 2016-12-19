33c1dfc6bf1d0390340cff7ecd33a09d972d9f0f
Joshua Rinard <jrinard@hotmail.com>
2016-12-19 15:34:27 -0800

    basic interface

---
+++ public/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+  $('.button-collapse').sideNav({
+        menuWidth: 300, // Default is 240
+        closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      }
+    );
+    $('.collapsible').collapsible();
+});
