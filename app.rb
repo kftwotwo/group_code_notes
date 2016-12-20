0133aff7c2817e894b1643c132d259bb0a75d3b4
Kevin Finley <kftwotwo@gmail.com>
2016-12-20 11:05:55 -0800

    able to add a new folder

--- app.rb
+++ app.rb
@@ -26,6 +26,31 @@ after do
   ActiveRecord::Base.clear_active_connections!
 end
 
+before do
+  @folders = Folder.all
+end
+
 get '/' do
   erb :index
 end
+
+post '/new_folder' do
+  folder_name = params[:name]
+  @folder = Folder.create(:name => folder_name)
+  erb(:index)
+end
+
+get '/folder/:id' do
+  @folder = Folder.find(params["id"].to_i)
+  @folder_name = Folder.first.name
+  erb(:folder)
+end
+
+delete '/folder/:id' do
+  Folder.find(params["id"].to_i).destroy
+  redirect '/'
+end
+
+# post('/new_snippet') do
+#
+# end
