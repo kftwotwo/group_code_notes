1ae8222fcaa7ef5951da186f55b2c9b2c2669b64
Diego Suarez <diego.spb@gmail.com>
2016-12-21 10:28:28 -0800

    fix button

--- app.rb
+++ app.rb
@@ -36,7 +36,7 @@ end
 
 post '/import_gists' do
   import_gists()
-  erb(:gists)
+  erb(:gists) 
 end
 
 get '/html' do
@@ -66,10 +66,7 @@ get '/' do
 end
 post '/new_folder' do
   folder_name = params[:name]
-  @folder_language = params['selected-language']
-  $language = @folder_language
-  @folder = Folder.create(:name => folder_name, :github_username => current_github_username, :language=> $language)
-
+  @folder = Folder.create(:name => folder_name, :github_username => current_github_username, :language=>"Ruby")
   redirect '/'
 end
 
@@ -100,6 +97,7 @@ post('/new_snippet') do
   new_snippet = Snippet.create(:title => snippet_title, :content => snippet_content, :tags => snippet_tags, :github_username => current_github_username, :language => "ruby",:description => snippet_description, :public => true)
   @folder = Folder.find(folder_id)
   @folder.snippets.push(new_snippet)
+  p @folder.snippets
   redirect '/'
 end
 
