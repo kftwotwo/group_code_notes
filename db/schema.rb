d2d2d1ee2140f3f267f613e33fb3dca02660ca86
Joshua Rinard <jrinard@hotmail.com>
2016-12-21 16:07:28 -0800

    add comments to database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161221082724) do
+ActiveRecord::Schema.define(version: 20161222000528) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20161221082724) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.text     "description"
+    t.text     "comments"
   end
 
 end
